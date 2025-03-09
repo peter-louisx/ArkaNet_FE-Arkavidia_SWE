03d5445efeb5df884356d46d5a1acc45326cc6ea
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-09 13:57:30 +0700

    feat: session handler

---
+++ src/lib/session.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+
+export const verifySession = async () => {
+    const cookieStore = await cookies();
+    const cookie = cookieStore.get("auth_token")?.value;
+  
+    let isAuthenticated = false;
+    let user = {};
+    if (!cookie) return { isAuthenticated, user };
+  
+    const session = await decrypt(cookie);
+    // const data = await getSession();
+
+  
+    if (session) {
+      isAuthenticated = true;
+    }
+  
+    return { isAuthenticated, user };
+};
+  
+
+export async function decrypt(cookie: string | undefined = "") {
+  try {
+    if (!cookie) {
+      throw "no cookie provided";
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.getAll().forEach((cookie) => {
+    cookieStore.delete(cookie.name);
+  });
+
+  cookieStore.delete({
+    name: "auth_token",
+    secure: true,
+    path: "/",
+  });
+  
+}
+
+// export async function getSession(auth_token?: string) {
+//   try {
+//     const cookieStore = await cookies();
+//     const token = auth_token ?? cookieStore.get("auth_token")?.value;
+
+//     if (!token) return null;
+
+
+//     const data = await resp.json();
+//     return resp.ok ? data : null;
+//   } catch (e) {
+//     // DO NOTHING
+//   }
+// }
