bd0d0927c525c264574807e55837362175da3ac2
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-10 22:37:48 +0700

    feat: authRoutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { verifySession } from './lib/session'
 
-
+const authRoutes = [
+  '/notifications',
+  '/chat',
+]
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
@@ -11,6 +14,10 @@ export async function middleware(request: NextRequest) {
 
   if(request.nextUrl.pathname === '/login' && isAuthenticated) {
     return NextResponse.redirect(new URL("/seeker/fsdf", request.nextUrl))
+  }
+
+  if(authRoutes.includes(request.nextUrl.pathname) && !isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl))
   }
 
   return NextResponse.next()
