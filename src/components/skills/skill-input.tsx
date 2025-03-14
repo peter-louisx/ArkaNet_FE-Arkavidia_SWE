e9213e9edb7681bd52553ba000a9235763962aea
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-15 01:25:53 +0700

    feat: dummy skills data

--- src/components/skills/skill-input.tsx
+++ src/components/skills/skill-input.tsx
@@ -17,128 +17,74 @@ export default function SkillInput({
 
   const skills = [
     {
-      id: "1",
+      id: "b9e31a75-2852-4a28-a2a2-c8d900396444",
       name: "React",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
     },
     {
-      id: "2",
-      name: "Vue",
+      id: "40be58e8-20f0-41e3-84ec-60dd97e7f92d",
+      name: "AngularJS",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
     },
     {
-      id: "3",
-      name: "Angular",
-    },
-    {
-      id: "4",
-      name: "Svelte",
-    },
-    {
-      id: "5",
-      name: "Ember",
-    },
-    {
-      id: "6",
-      name: "Backbone",
-    },
-    {
-      id: "7",
-      name: "Meteor",
-    },
-    {
-      id: "8",
-      name: "Aurelia",
-    },
-    {
-      id: "9",
-      name: "Polymer",
-    },
-    {
-      id: "10",
-      name: "Knockout",
-    },
-    {
-      id: "11",
-      name: "Vanilla JS",
-    },
-    {
-      id: "12",
-      name: "jQuery",
-    },
-    {
-      id: "13",
-      name: "Lodash",
-    },
-    {
-      id: "14",
-      name: "Underscore",
-    },
-    {
-      id: "15",
-      name: "Ramda",
-    },
-    {
-      id: "16",
-      name: "Redux",
-    },
-    {
-      id: "17",
-      name: "MobX",
-    },
-    {
-      id: "18",
-      name: "RxJS",
-    },
-    {
-      id: "19",
-      name: "Ngrx",
-    },
-    {
-      id: "20",
-      name: "Vuex",
-    },
-    {
-      id: "21",
-      name: "React Router",
-    },
-    {
-      id: "22",
-      name: "Vue Router",
-    },
-    {
-      id: "23",
-      name: "Angular Router",
-    },
-    {
-      id: "24",
-      name: "Svelte Router",
-    },
-    {
-      id: "25",
-      name: "Ember Router",
-    },
-    {
-      id: "26",
-      name: "Backbone Router",
-    },
-    {
-      id: "27",
-      name: "Meteor Router",
-    },
-    {
-      id: "28",
-      name: "Aurelia Router",
-    },
-    {
-      id: "29",
-      name: "Polymer Router",
-    },
-    {
-      id: "30",
-      name: "Knockout Router",
-    },
-    {
-      id: "31",
+      id: "cfa78db7-67c1-4886-8709-b81dfd3abf99",
       name: "AWS",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "108d1dcc-7541-4b40-885d-b2632aab79b7",
+      name: "Docker",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "f6c96854-99f6-474d-a52c-0a7224afa2e8",
+      name: "TypeScript",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "4509850d-4ead-4fda-8439-e6111c0962c0",
+      name: "Next.js",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "e043b5f1-60c7-41f8-b7ab-bb731e9b2de1",
+      name: "Python",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "6bf0a983-7a35-4e46-996f-5de89bd2822b",
+      name: "Docker",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "55b6ca9a-64b2-4a78-a22a-01baa056e1c0",
+      name: "Kubernetes",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
+    },
+    {
+      id: "87e4f075-2cab-4a48-b63e-eaca97cf082a",
+      name: "CI/CD",
+      created_at: null,
+      updated_at: null,
+      deleted_at: null,
     },
   ];
 
