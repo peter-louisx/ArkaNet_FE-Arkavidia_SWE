5877bb361e3ea78e3700c678cdbfb1520b7b4904
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-16 21:37:08 +0700

    fix: update naming

--- src/lib/image-placeholder.ts
+++ src/lib/image-placeholder.ts
@@ -1,5 +1,5 @@
-const PROFILE_PICTURE = "/profile-picture/placeholder.png";
-const HEADER_PICTURE = "/header-picture/placeholder.png";
-const COMPANY_PICTURE = "/company-picture/placeholder.png";
+const PROFILE_PICTURE = "/profile/placeholder.png";
+const HEADER_PICTURE = "/header/placeholder.png";
+const COMPANY_PICTURE = "/companyplaceholder.png";
 
 export { PROFILE_PICTURE, HEADER_PICTURE, COMPANY_PICTURE };
