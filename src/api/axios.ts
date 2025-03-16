804cd232a6de815f5a810d0a2067c0f612e3ffa9
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-03-16 10:01:26 +0700

    feat: axios intercept 401 error

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import api from "./api";
+import { deleteSession } from "@/lib/session";
 
 const axios = Axios.create({
   baseURL: api.BASE_URL_API,
@@ -8,5 +9,24 @@ const axios = Axios.create({
     Accept: "application/json",
   },
 });
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (
+      error?.response?.status === 401 &&
+      window?.location?.pathname !== "/login"
+    ) {
+
+      await deleteSession();
+      localStorage.clear();
+      return window.location.replace("/login");
+    }
+
+    throw error;
+  }
+);
 
 export default axios;
